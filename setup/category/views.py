e81a6382831276e9bb61f118be9720c7030eac82
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-09-14 21:04:01 +0300

    ending filters for products category

--- setup/category/views.py
+++ setup/category/views.py
@@ -1,12 +1,68 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import Http404
 from .models import *
+from django.db.models import FilteredRelation, Q
 def products_by_category(request, slug ):
+    # print("low" in request.GET["args"])
+
+    print(request.path)
     template = "category/category_products.html"
     category = get_object_or_404 (Category, slug = slug)
     products = Products.objects.filter(category = category)
+    section = "all"
+
+    try:
+        if "low" in request.GET["args"]:
+            products =Products.objects.filter(category = category).order_by("price")
+            section = "low"
+        elif "hight" in request.GET["args"]:
+            products =Products.objects.filter(category = category).order_by("-price")
+            section = "hight"
+        elif "popularity" in request.GET["args"]:
+            products = Products.objects.filter(category = category).prefetch_related("productRaiting").order_by("-productRaiting")
+            section = "popularity"
+
+        raiting_filters = {
+            "starone":Products.objects.filter(
+                category = category
+                ).annotate(
+                    products = FilteredRelation(
+                        "productRaiting" 
+                        )
+                    ).filter(products__raiting = 1),
+            "startwo":Products.objects.filter(
+                category = category
+                ).annotate(
+                    products = FilteredRelation(
+                        "productRaiting" 
+                        )
+                    ).filter(products__raiting = 2),
+            "starthree":Products.objects.filter(
+                category = category
+                ).annotate(
+                    products = FilteredRelation(
+                        "productRaiting" 
+                        )
+                    ).filter(products__raiting = 3),
+            "starfour":Products.objects.filter(
+                category = category
+                ).annotate(
+                    products = FilteredRelation(
+                        "productRaiting" 
+                        )
+                    ).filter(products__raiting = 4),
+        }
+        for key, values in raiting_filters.items():
+            if key == request.GET["args"]:
+                products = values
+                section = key
+
+    except:
+        pass
+
     context = {
-        "products":products
+        "products":products,
+        "section":section,
     }
 
     return render(request, template, context)
